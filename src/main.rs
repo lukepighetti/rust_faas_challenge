8265d74bbab17e889a62c1bfff80bb89cb4bb35f
Luke Pighetti <lukepighetti@gmail.com>
2022-02-12 14:32:36 -0500

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use serde::{Deserialize, Serialize};
+
 fn main() {
-    println!("Hello, world!");
+    let x: i32 = 1;
+    let mut y: i32 = 2;
+    println!("{} {}", x, y);
+    y = 3;
+    println!("{:?} {}", x, y);
+
+    let four_ints: [i32; 4] = [1, 2, 3, 4];
+
+    println!("{:?}", four_ints);
+
+    println!("Hello world");
+
+    let me = Person {
+        name: String::from("Luke"),
+        age: 33,
+    };
+
+    me.greet();
+
+    println!("{:?} {:?}", me.name, me.age);
+
+    let serialized_user = serde_json::to_string(&me).unwrap();
+
+    println!("{:?}", serialized_user);
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Person {
+    name: String,
+    age: i32,
+}
+
+trait Greeter {
+    fn greet(&self) -> ();
+}
+
+impl Greeter for Person {
+    fn greet(&self) -> () {
+        println!("Hellllllooooooo, I'm {}", self.name);
+    }
 }
