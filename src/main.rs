e5572725f63f5a859d6da9efe57c96f7cf40731c
Luke Pighetti <lukepighetti@gmail.com>
2022-02-13 08:36:11 -0500

    serialization

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,20 @@
 use serde::{Deserialize, Serialize};
 
 fn main() {
-    let x: i32 = 1;
-    let mut y: i32 = 2;
-    println!("{} {}", x, y);
-    y = 3;
-    println!("{:?} {}", x, y);
-
-    let four_ints: [i32; 4] = [1, 2, 3, 4];
-
-    println!("{:?}", four_ints);
-
-    println!("Hello world");
-
     let me = Person {
         name: String::from("Luke"),
         age: 33,
+        hobbies: Vec::from([
+            Hobby {
+                name: String::from("Woodworking"),
+                years: 20,
+            },
+            Hobby {
+                name: String::from("Motorcycling"),
+                years: 10,
+            },
+        ]),
     };
-
-    me.greet();
-
-    println!("{:?} {:?}", me.name, me.age);
 
     let serialized_user = serde_json::to_string(&me).unwrap();
 
@@ -31,14 +25,11 @@ fn main() {
 struct Person {
     name: String,
     age: i32,
+    hobbies: Vec<Hobby>,
 }
 
-trait Greeter {
-    fn greet(&self) -> ();
-}
-
-impl Greeter for Person {
-    fn greet(&self) -> () {
-        println!("Hellllllooooooo, I'm {}", self.name);
-    }
+#[derive(Serialize, Deserialize, Debug)]
+struct Hobby {
+    name: String,
+    years: i32,
 }
